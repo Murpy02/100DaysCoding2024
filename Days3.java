9f673c816e1046c558831e9e91c7797e83d3bfe9
Murpy02 <murpy02022005@gmail.com>
2024-09-04 17:24:15 +0800

    Add files via upload

---
+++ Days3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DaysOfCoding;
+
+
+public class Days3 {
+    public static void main(String[] args){
+    String Nama = "Murpy";
+    double Tinggi_Badan = 168;
+    int Berat_Badan = 65;
+    int Umur = 19;
+    boolean isIstudent = true;
+    
+    System.out.println("Nama: " +Nama);
+    System.out.println("Tinggi_Badan: " +Tinggi_Badan);
+    System.out.println("Berat_Badan: " +Berat_Badan);
+    System.out.println("Umur: " +Umur);
+    
+    
+    
+    
+    }    
+    
+}
