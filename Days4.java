0bf0cbf24e85a33afdc4047be1b49caab9286fa4
Murpy02 <murpy02022005@gmail.com>
2024-09-05 20:19:01 +0800

    Add files via upload

---
+++ Days4.java
@@ -0,0 +1,19 @@
+
+package DaysOfCoding;
+
+
+public class Days4 {
+    public static void main(String[] args) {
+        String a = "semangat";
+        int b = 45;
+        
+        System.out.printf("%s%n",a);
+        System.out.printf("%d%n",b);
+        
+        System.out.print("Semangat");
+        System.out.print("45");
+        
+        
+    }
+    
+}
