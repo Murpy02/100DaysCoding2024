98e21d31e6e7fd8a91374592b7e7b85f9bc63e7f
Murpy02 <murpy02022005@gmail.com>
2024-09-06 19:14:47 +0800

    Add files via upload

---
+++ Days5.java
@@ -0,0 +1,15 @@
+
+package DaysOfCoding;
+
+
+public class Days5 {
+    public static void main(String[] args) {
+        /**
+         * metode ini digunakan untuk menampilkan pesan "Hello, World!" di layar
+         */
+    System.out.println("Hello, World!");
+    
+    }
+    
+    
+}
