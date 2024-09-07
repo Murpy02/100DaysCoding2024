301761c0b1c5df47ec2674d113ffcd004effd1da
Murpy02 <murpy02022005@gmail.com>
2024-09-07 20:02:46 +0800

    Add files via upload

---
+++ Days6.java
@@ -0,0 +1,14 @@
+
+package DaysOfCoding;
+
+
+public class Days6 {
+    public static void main(String[] args) {
+        String nama = "Bang Mur";
+        double tinggi = 1.86;
+        
+        System.out.println("nama: " +nama);
+        System.out.println("tinggi: " +tinggi);
+    }
+    
+}
