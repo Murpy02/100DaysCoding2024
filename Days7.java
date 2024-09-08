d4744d7ff36cd9f0b6a20f3138856ad778228f94
Murpy02 <murpy02022005@gmail.com>
2024-09-08 19:28:57 +0800

    Add files via upload

---
+++ Days7.java
@@ -0,0 +1,16 @@
+
+package DaysOfCoding;
+
+
+public class Days7 {
+    public static void main(String[] args) {
+        String hobby;
+        hobby = "football";
+        int umur;
+        umur = 18;
+        
+        System.out.println("Hobby: " +hobby);
+        System.out.println("umur: " +umur);
+        
+    }
+}
