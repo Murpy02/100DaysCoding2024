86415f633c456cda2591e9248d16b46f3fa73549
Murpy02 <murpy02022005@gmail.com>
2024-09-09 20:30:50 +0800

    Add files via upload

---
+++ Days8.java
@@ -0,0 +1,13 @@
+
+package DaysOfCoding;
+
+
+public class Days8 {
+    public static void main(String[] args) {
+        String nama="gass";
+        
+        nama="bangMur";
+        
+        System.out.println("nama: " +nama);
+    }
+}
