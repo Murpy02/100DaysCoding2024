42bc15043d1de0c2f3fc75a695167cd4871f9bc7
Murpy02 <murpy02022005@gmail.com>
2024-09-15 19:57:17 +0800

    Add files via upload

---
+++ Days14.java
@@ -0,0 +1,11 @@
+
+package DaysOfCoding;
+
+
+public class Days14 {
+    public static void main(String[] args) {
+        long jarak_Antar_Bintang = 2300000L;
+        
+        System.out.println("jarak_Antar_Bintang: " +jarak_Antar_Bintang+ " Km");
+    }
+}
