8ecd1f0b094c66b54664bfccbc148d63bb425ec1
Murpy02 <murpy02022005@gmail.com>
2024-09-16 19:47:02 +0800

    Add files via upload

---
+++ Days15.java
@@ -0,0 +1,11 @@
+
+package DaysOfCoding;
+
+
+public class Days15 {
+    public static void main(String[] args) {
+        double gaji = 200.000;
+        
+        System.out.println(gaji);
+    }
+}
