78c61d3511622312dc8f2dddc31f5892a563e996
Murpy02 <murpy02022005@gmail.com>
2024-09-17 20:10:28 +0800

    Add files via upload

---
+++ Days16.java
@@ -0,0 +1,11 @@
+
+package DaysOfCoding;
+
+
+public class Days16 {
+    public static void main(String[] args) {
+        float tinggi = 3.75f;
+        
+        System.out.println("tinggi: "+tinggi+ " m" );
+    }
+}
