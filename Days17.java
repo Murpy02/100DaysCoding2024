d390524dc1f7e833e452e1a62907f42a627ffb94
Murpy02 <murpy02022005@gmail.com>
2024-09-18 19:59:48 +0800

    Add files via upload

---
+++ Days17.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days17 {
+    public static void main(String[] args) {
+        char ganjil = '3';
+        
+        System.out.println(ganjil);
+    }
+    
+}
