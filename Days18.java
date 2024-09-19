26edcc7ae7bb6425ec3639e7ea8ee69d685b7879
Murpy02 <murpy02022005@gmail.com>
2024-09-19 18:33:38 +0800

    Add files via upload

---
+++ Days18.java
@@ -0,0 +1,11 @@
+
+package DaysOfCoding;
+
+
+public class Days18 {
+    public static void main(String[] args) {
+        boolean isStudent= true;
+        
+        System.out.println("isStudent: " +isStudent);
+    }
+}
