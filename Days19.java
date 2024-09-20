918cd285aa475e7691d08c1450aa6b763cf57ae9
Murpy02 <murpy02022005@gmail.com>
2024-09-20 21:50:02 +0800

    Add files via upload

---
+++ Days19.java
@@ -0,0 +1,20 @@
+
+package DaysOfCoding;
+
+
+public class Days19 {
+    public static void main(String[] args) {  
+       String NAMA = "Murpy";
+       String NIM = "D0224004";
+       String KELAS = "Informatika A";
+       
+      String Gabungan = NAMA + " " + NIM + " " + KELAS;
+        System.out.println(Gabungan);
+        
+    }
+    
+    
+    
+    
+    
+}
