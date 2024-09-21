7de085a4102e416a0f750079f60b69e5da818082
Murpy02 <murpy02022005@gmail.com>
2024-09-22 06:35:15 +0800

    Add files via upload

---
+++ Days20.java
@@ -0,0 +1,12 @@
+package DaysOfCoding;
+
+public class Days20 {
+
+    public static void main(String[] args) {
+
+        byte ukuranBaju = 32;
+
+        System.out.println("Ukuran Baju : " + ukuranBaju);
+
+    }
+}
