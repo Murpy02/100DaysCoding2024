5c4629f1a0cc299efb56bac7901da8165bead1ad
Murpy02 <murpy02022005@gmail.com>
2024-09-22 18:47:17 +0800

    Add files via upload

---
+++ Days21.java
@@ -0,0 +1,17 @@
+
+package DaysOfCoding;
+
+
+public class Days21 {
+    public static void main(String[] args) {
+        short panjang = 12;
+        short lebar = 4;
+        
+        System.out.println("Panjang :"+panjang);
+        System.out.println("Lebar :"+lebar);
+        
+        
+       
+    }
+    
+}
