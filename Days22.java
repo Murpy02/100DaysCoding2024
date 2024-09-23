413e2c1737ee1b49bb25fb45a93355a032a75788
Murpy02 <murpy02022005@gmail.com>
2024-09-23 19:36:31 +0800

    Add files via upload

---
+++ Days22.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DaysOfCoding;
+
+/**
+ *
+ * @author Murpy
+ */
+public class Days22 {
+    public static void main(String[] args) {
+        Integer tahun = 2024;//non primitif
+        Integer tanggal = 23;
+        int bulan = 9; // primitif
+        
+        System.out.println(tanggal+ " / " + bulan + " / " + tahun);
+        
+        
+    }
+        
+        
+    }
+       
+      
+    
+            
+    
