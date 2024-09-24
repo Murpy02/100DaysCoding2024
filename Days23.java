195ffd11fcf28631c027c4340124ea61eaa01e7a
Murpy02 <murpy02022005@gmail.com>
2024-09-24 18:28:37 +0800

    Add files via upload

---
+++ Days23.java
@@ -0,0 +1,12 @@
+package DaysOfCoding;
+
+public class Days23 {
+
+    public static void main(String[] args) {
+        long Jumlah_Penduduk = 120000L;
+
+        System.out.println(" jumlah penduduk : " + Jumlah_Penduduk + " Jiwa ");
+
+    }
+
+}
