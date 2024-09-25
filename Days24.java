a44ad770dce6df5c9e01e9dfaae89f28bd9d711a
Murpy02 <murpy02022005@gmail.com>
2024-09-25 21:43:15 +0800

    Add files via upload

---
+++ Days24.java
@@ -0,0 +1,13 @@
+
+package DaysOfCoding;
+
+
+public class Days24 {
+    public static void main(String[] args) {
+        float Tinggi= 12.6f;
+        float Berat = 25.34f;
+        
+        System.out.println(" Tinggi : " + Tinggi);
+        System.out.println(" Tinggi : " + Berat );
+    }
+}
