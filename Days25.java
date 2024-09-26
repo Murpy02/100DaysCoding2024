12ea93c62670b4290d8a4f04eee1bc8f33dc1292
Murpy02 <murpy02022005@gmail.com>
2024-09-26 21:27:37 +0800

    Add files via upload

---
+++ Days25.java
@@ -0,0 +1,11 @@
+package DaysOfCoding;
+
+public class Days25 {
+
+    public static void main(String args[]) {
+        Double berat = 30.5; // merujuk ke objek Double yang menyimpan nilai 30.5.
+
+        System.out.println(" Berat Badan : " + berat);
+    }
+
+}
