dee46515572ec47db7839b3c421ff2778ead59d2
Murpy02 <murpy02022005@gmail.com>
2024-09-27 21:12:19 +0800

    Add files via upload

---
+++ Days26.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days26 {   
+    public static void main( String args[]){
+        Character Inisial= 'A';//merujuk ke objek Character yang menyimpan nilai A.                          
+        
+        System.out.println("Inisial : "+ Inisial);
+    }
+    
+}
