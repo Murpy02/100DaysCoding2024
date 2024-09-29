d9a6f9f1f37bfc60c71177c62d6252429a1c7804
Murpy02 <murpy02022005@gmail.com>
2024-09-29 21:02:01 +0800

    Add files via upload

---
+++ Days28.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days28 {
+    public static void main ( String[] args ){
+        Boolean suka = false;
+        
+        System.out.println(" Suka : " +suka);
+    }
+    
+}
