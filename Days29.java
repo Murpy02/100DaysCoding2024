c04d5c9b9f67747c40e9cc18a0867b03365abb3a
Murpy02 <murpy02022005@gmail.com>
2024-09-30 19:14:11 +0800

    Add files via upload

---
+++ Days29.java
@@ -0,0 +1,11 @@
+
+package DaysOfCoding;
+import java.math.BigInteger;
+public class Days29 {
+    public static void main (String[] args){
+        BigInteger jumlah = new BigInteger("12345678901234567890");
+        
+        System.out.println( " Jumlah Orang di bumi ini  : " + jumlah );
+    }
+}
+    
