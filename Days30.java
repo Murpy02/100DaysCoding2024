e472049596ac34ec49a928bf4886b9334d405f7d
Murpy02 <murpy02022005@gmail.com>
2024-10-01 21:48:11 +0800

    Add files via upload

---
+++ Days30.java
@@ -0,0 +1,12 @@
+package DaysOfCoding;
+
+import java.math.BigDecimal;
+
+public class Days30 {
+
+    public static void main(String[] args) {
+        BigDecimal jumlahKekayaan = new BigDecimal("12345678901234567890");
+
+        System.out.printf("Jumlah kekayaan Bang Mur adalah %f", jumlahKekayaan);
+    }
+}
