b6e0adaef331a91c16bd3e51344624ef3f9a892e
Murpy02 <murpy02022005@gmail.com>
2024-10-02 20:40:25 +0800

    Add files via upload

---
+++ Days31.java
@@ -0,0 +1,13 @@
+
+package DaysOfCoding;
+
+
+public class Days31 {
+     public static void main(String[] args) {
+        String umur = "29";
+        
+        int usia = Integer.parseInt(umur);
+         System.out.println("Uisa : " + usia + " tahun");
+    }
+    
+}
