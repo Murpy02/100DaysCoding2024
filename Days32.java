6f458216ccb07ac552ac6ceb9010006d89a5f689
Murpy02 <murpy02022005@gmail.com>
2024-10-03 19:50:35 +0800

    Add files via upload

---
+++ Days32.java
@@ -0,0 +1,16 @@
+
+package DaysOfCoding;
+
+
+public class Days32 {
+    public static void main ( String[]  args){
+        int panjang = 123;
+        
+        String lebar = String.valueOf(panjang);
+        System.out.println( " Lebar : " + lebar + " cm " );
+        
+        
+     
+    }
+    
+}
