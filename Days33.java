87ed3331cb27439245009b10464959e401e150ef
Murpy02 <murpy02022005@gmail.com>
2024-10-04 19:43:16 +0800

    Add files via upload

---
+++ Days33.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days33 {
+    public static void main ( String[] args){
+        int a = 12;
+        double b =a;
+        System.out.println(b);
+    }
+    
+}
