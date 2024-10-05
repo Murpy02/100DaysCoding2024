c98ef31bc18c1785edb3469a00282d18932e3350
Murpy02 <murpy02022005@gmail.com>
2024-10-05 21:06:29 +0800

    Add files via upload

---
+++ Days34.java
@@ -0,0 +1,20 @@
+package DaysOfCoding;
+
+import java.util.Scanner;
+
+public class Days34 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan nama : ");
+        String nama = input.nextLine();
+        System.out.println(" nama saya : " + nama);
+
+        System.out.println(" Masukkan umur : ");
+        int umur = input.nextInt();
+        System.out.println("Umur saya : " + umur + " Tahun");
+
+    }
+
+}
