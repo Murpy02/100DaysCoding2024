e59ae0c2449626de47254db86663ee1c55b46b5c
Murpy02 <murpy02022005@gmail.com>
2024-10-06 19:25:21 +0800

    Add files via upload

---
+++ Days35.java
@@ -0,0 +1,19 @@
+
+package DaysOfCoding;
+
+
+public class Days35 {
+    public static void main (String[] args){
+        
+        int a = 25;
+        int b = 35;
+        
+        //penjumlahan
+        int c = a +  b;//25 + 35 = 60
+        //pengurangan
+        int d = b - a;//35 - 25 = 10
+        
+        System.out.println(" Hasil Penjumlahan : " + c);
+        System.out.println(" Hasil Pengurangan : " + d);
+    }
+}
