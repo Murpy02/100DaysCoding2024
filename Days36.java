73ab333af472956a0555cd04ebde4ad9ebafb9d0
Murpy02 <murpy02022005@gmail.com>
2024-10-07 18:22:18 +0800

    Add files via upload

---
+++ Days36.java
@@ -0,0 +1,23 @@
+
+package DaysOfCoding;
+
+
+public class Days36 {
+    public static void main (String[] args){
+        int a = 10;
+        int b = 5;
+        
+        //perkalian
+        int perkalian = a * b;// 10 * 5 = 50
+        //pembagian
+        int pembagian = a / b;// 10 / 5 = 2
+        //modulo
+        int modulo = a % b;// 10 % 5 = 0 ( sisa pembagian 10 dibagi 5 adalah 0 )
+        
+        System.out.println( " Hasil Perkalian : " + perkalian);
+        System.out.println( " Hasil Pembagian : " + pembagian);
+        System.out.println( " Hasil Modulo : " + modulo);
+                
+    }
+    
+}
