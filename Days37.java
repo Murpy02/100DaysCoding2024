235d4cc2e5039f938f371aa2e76ba079d13e5e74
Murpy02 <murpy02022005@gmail.com>
2024-10-08 20:32:22 +0800

    Add files via upload

---
+++ Days37.java
@@ -0,0 +1,20 @@
+package DaysOfCoding;
+
+public class Days37 {
+
+    public static void main(String[] args) {
+        //Operator pengisian nilai
+        int a = 4;//mengisi variabel a dengan nilai 4
+        int b = 2;//mengisi variabel b dengan nilai 2
+        System.out.println(a);
+        System.out.println(b);
+
+        // Operator Pengisian dan Penambahan
+        a += b;
+        System.out.println("Hasil penambahan : " + a);
+
+        //Operaator Pengisian dan Pengurangan
+        a -= b;
+        System.out.println("Hasil Pengurangan : " + a);
+    }
+}
