c7cdf3e360cde3d1d59eaf16a7a1be3347b6f4dd
Murpy02 <murpy02022005@gmail.com>
2024-10-09 20:41:03 +0800

    Add files via upload

---
+++ Days38.java
@@ -0,0 +1,18 @@
+
+package DaysOfCoding;
+
+
+public class Days38 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        //Operator Pengisian dan Perkalian
+        a *= b;
+        System.out.println(" Hasil Perkalian : " + a);
+
+        //Operator Pengisian dan Pembagian
+        a /= b;
+        System.out.println(" Hasil Pembagian : " + a);
+    }
+}
