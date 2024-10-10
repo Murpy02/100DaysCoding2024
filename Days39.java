877f28be1ff747c9b3480ed74a0b70ed5b8c397f
Murpy02 <murpy02022005@gmail.com>
2024-10-10 11:57:06 +0800

    Add files via upload

---
+++ Days39.java
@@ -0,0 +1,14 @@
+
+package DaysOfCoding;
+
+
+public class Days39 {
+    public static void main(String[] args){
+        int a =20;
+        int b = 18;
+        //Pengisian dan Modolu
+        a %= b;
+        System.out.println("Hasil Modulo : " +a);
+    }
+    
+}
