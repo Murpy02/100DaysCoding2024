a9698531efe9f6e9dbc75532fa1551aaa27cd37b
Murpy02 <murpy02022005@gmail.com>
2024-10-11 09:06:49 +0800

    Add files via upload

---
+++ Days40.java
@@ -0,0 +1,15 @@
+package DaysOfCoding;
+
+public class Days40 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        boolean c;
+        //Operator Perbandingan : lebih besar dan  lebih kecil
+        c = a > b;//apakah a =20 lebih besar dari b=40
+        System.out.println("Hasil a > b : " + c);
+        c = a < b;//apakah a =20 lebih kecil dari b=40
+        System.out.println("Hasil a < b : " + c);
+    }
+}
