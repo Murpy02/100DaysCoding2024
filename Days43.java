5ed233cee80a875f6aae8585a3d8eb14923f21ef
Murpy02 <murpy02022005@gmail.com>
2024-10-14 20:59:26 +0800

    Add files via upload

---
+++ Days43.java
@@ -0,0 +1,16 @@
+
+package DaysOfCoding;
+
+
+public class Days43 {
+    public static void main(String[] args) {
+        int a =5;
+        int b =4;
+        
+        //operator perbandingan lebih besar sama dengan
+        System.out.println(a>=b);
+         //operator perbandingan lebih kecil sama dengan
+         System.out.println(a<=b);
+    }
+    
+}
