5a59b2f04259ff704513122a69192631ec6d494b
Murpy02 <murpy02022005@gmail.com>
2024-10-15 22:39:39 +0800

    Add files via upload

---
+++ Days44.java
@@ -0,0 +1,14 @@
+
+package DaysOfCoding;
+
+
+public class Days44 {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true;
+        boolean c;
+        
+        c = a && b ;
+        System.out.println(c);
+    }
+}
