90e1df344f32669d6f55146ca87591d0143cbcae
Murpy02 <murpy02022005@gmail.com>
2024-10-18 20:45:15 +0800

    Add files via upload

---
+++ Days47.java
@@ -0,0 +1,15 @@
+
+package DaysOfCoding;
+
+
+public class Days47 {
+    public static void main(String[] args){
+        int a =45;
+        int b = 34;
+        
+        
+        if ( a > b){//percabangan if
+            System.out.println("betul");
+        }
+    }
+}
