ec1075578c44154c00ccb4baa196c1abc1e58290
Murpy02 <murpy02022005@gmail.com>
2024-10-19 20:49:07 +0800

    Add files via upload

---
+++ Days48.java
@@ -0,0 +1,18 @@
+
+package DaysOfCoding;
+
+
+public class Days48 {
+    public static void main(String[] args){
+        int a = 23;
+        int b = 45;
+        
+//        percabangan if-else
+        if ( a > b ){
+            System.out.println(" betul");
+        } else {
+            System.out.println(" Salah ");
+        }
+    }
+    
+}
