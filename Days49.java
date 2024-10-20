3b2489a8ca16e46db6faedbc848897b74d856cab
Murpy02 <murpy02022005@gmail.com>
2024-10-20 20:05:58 +0800

    Add files via upload

---
+++ Days49.java
@@ -0,0 +1,22 @@
+
+package DaysOfCoding;
+
+
+public class Days49 {
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 35;
+        
+//        Percabangan : if- else if
+        if ( a < b){
+            System.out.println(a + " lebih kecil dari" + b);
+        }else if ( a > b){
+            System.out.println(a +" lebih besar dari " + b);
+        }else if ( a == b){
+            System.out.println(a +"sama dengan"+b);
+        } else {
+            System.out.println(a+"tidak sama dengan"+b);
+            
+        }
+    }
+}
