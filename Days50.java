53bd2c0aa81771166403792f054674ec4f92d6e8
Murpy02 <murpy02022005@gmail.com>
2024-10-21 19:23:27 +0800

    Add files via upload

---
+++ Days50.java
@@ -0,0 +1,18 @@
+
+package DaysOfCoding;
+
+
+public class Days50 {
+    public static void main(String[] args) {
+       int umur = 18;
+       
+//       Percabangan : if- else if - else
+       if ( umur >= 18 && umur <= 25){
+           System.out.println("Anda masih muda");
+       }else if ( umur >= 25 && umur <= 50){
+           System.out.println("Anda sudah dewasa");
+       } else {
+           System.out.println("Pengalaman anda sudah banyak");
+       }
+}
+}
