e4a0eb08f6fd6af407a2577b91fa6c5b1b3739b3
Murpy02 <murpy02022005@gmail.com>
2024-10-23 19:32:28 +0800

    Add files via upload

---
+++ Days52.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days52 {
+    public static void main(String[] args) {
+        int nilai =75;
+        String hasil = (nilai >= 70) ? "Lulus":"Tidak Lulus";
+        System.out.println(hasil);
+    }
+    
+}
