6e546120519659e5c5921d2df1088e822a3965de
Murpy02 <murpy02022005@gmail.com>
2024-10-26 22:23:32 +0800

    Add files via upload

---
+++ Days55.java
@@ -0,0 +1,14 @@
+
+package DaysOfCoding;
+
+
+public class Days55 {
+    public static void main(String[] args) {
+            for(int i=3;i>0;i--){
+        System.out.println(i+"> tetap semangat");
+            
+            }
+        
+    }
+    
+}
