c3e624ef0fdbf407972d1401fbb0f525e42a35b5
Murpy02 <murpy02022005@gmail.com>
2024-10-30 20:05:23 +0800

    Add files via upload

---
+++ Days59.java
@@ -0,0 +1,14 @@
+
+package DaysOfCoding;
+
+
+public class Days59 {
+    public static void main(String[] args) {
+        int angka []= {29,10,2024};
+        
+        for(int m : angka){
+            System.out.print(m + " ");
+    }
+ 
+}
+}
