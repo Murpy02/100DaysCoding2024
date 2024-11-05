3f9c3e25b05db00bab71e903ec72a5cfbc3f8083
Murpy02 <murpy02022005@gmail.com>
2024-11-05 21:04:20 +0800

    Add files via upload

---
+++ Days65.java
@@ -0,0 +1,13 @@
+
+package DaysOfCoding;
+
+
+public class Days65 {
+    public static void main(String[] args) {
+        for ( int i =1 ; i <=3 ;i++){
+            for(int j = 1 ; j <= 2 ;j++){
+                System.out.println("loop luar i : " + i+", loop dalam j : "+ j);
+            }
+        }
+    }
+}
