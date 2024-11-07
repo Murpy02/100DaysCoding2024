cfb0defae54cf56e6e4e77228790294a8e4d2d91
Murpy02 <murpy02022005@gmail.com>
2024-11-07 23:44:11 +0800

    Add files via upload

---
+++ Days67.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days67 {
+    public static void main(String[] args) {
+     print();
+    }
+    public static void print(){
+        System.out.println("Selamat malam");
+    }
+}
