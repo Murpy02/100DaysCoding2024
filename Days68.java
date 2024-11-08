c36d9789a773f51825f87501ec3fc6478a5e3c97
Murpy02 <murpy02022005@gmail.com>
2024-11-08 21:55:55 +0800

    Add files via upload

---
+++ Days68.java
@@ -0,0 +1,13 @@
+
+package DaysOfCoding;
+
+
+public class Days68 {
+    public static  int jumlah(int a , int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil =jumlah(10,20);
+        System.out.println("hasil penjumlahan : " + hasil);
+    }
+}
