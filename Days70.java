dc364ae2ccf117dd12d46c4580f6ead89639107d
Murpy02 <murpy02022005@gmail.com>
2024-11-10 18:40:34 +0800

    Add files via upload

---
+++ Days70.java
@@ -0,0 +1,16 @@
+
+package DaysOfCoding;
+
+
+public class Days70 {
+    public static int tambah (int b, int c){
+        return b + c;
+    }
+        public static void main(String[] args) {
+        int total = tambah (10, 5);
+        
+        
+            System.out.println("10 + 5 = "+ total);
+    }
+
+}
