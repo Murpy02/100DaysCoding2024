11f8ee1b9b11f4a5a8ccbd1e9d3a167d16f19dbc
Murpy02 <murpy02022005@gmail.com>
2024-11-11 20:23:48 +0800

    Add files via upload

---
+++ Days71.java
@@ -0,0 +1,17 @@
+package DaysOfCoding;
+
+public class Days71 {
+
+    public static int hitung(int sisi) {
+        return sisi * sisi;
+
+    }
+
+    public static void main(String[] args) {
+        int sisi = 2;
+        int luas = hitung(sisi);
+
+        System.out.println("luas persegi " + sisi + " adalah " + luas);
+
+    }
+}
