996c7b8c9b15a8433a3f4701bf963db2ed1402b4
Murpy02 <murpy02022005@gmail.com>
2024-11-13 21:57:33 +0800

    Add files via upload

---
+++ Days73.java
@@ -0,0 +1,12 @@
+package DaysOfCoding;
+
+public class Days73 {
+
+    public static void main(String[] args) {
+        String nama = "Dimas";
+
+        char inisial = nama.charAt(0);
+
+        System.out.println("Inisial " + inisial);
+    }
+}
