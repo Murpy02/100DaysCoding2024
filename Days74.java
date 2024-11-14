03519a77426509ffd7491753cf4e88cfbd00a0c5
Murpy02 <murpy02022005@gmail.com>
2024-11-14 20:30:39 +0800

    Add files via upload

---
+++ Days74.java
@@ -0,0 +1,11 @@
+package DaysOfCoding;
+
+public class Days74 {
+
+    public static void main(String[] args) {
+        String hobby = "FUTSAL";
+
+        int panjang = hobby.length();
+        System.out.println("Panjang kalimat : " + panjang);
+    }
+}
