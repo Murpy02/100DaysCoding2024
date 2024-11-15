e94647b077dadc07f038012362dc27e229ae5cd1
Murpy02 <murpy02022005@gmail.com>
2024-11-15 18:04:21 +0800

    Add files via upload

---
+++ Days75.java
@@ -0,0 +1,11 @@
+package DaysOfCoding;
+
+public class Days75 {
+
+    public static void main(String[] args) {
+        String teks = " TEKNIK INFORMATIKA";
+
+        String hasil = teks.toLowerCase();
+        System.out.println("teks dalam huruf kecil : " + hasil);
+    }
+}
