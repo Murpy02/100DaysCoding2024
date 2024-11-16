08edeb0cb988572e080879931d0d17f9f41cb9c7
Murpy02 <murpy02022005@gmail.com>
2024-11-16 21:45:58 +0800

    Add files via upload

---
+++ Days76.java
@@ -0,0 +1,11 @@
+package DaysOfCoding;
+
+public class Days76 {
+
+    public static void main(String[] args) {
+        String teks = "indonesia";
+
+        String hasil = teks.toUpperCase();
+        System.out.println("teks dalam huruf besar :> " + hasil);
+    }
+}
