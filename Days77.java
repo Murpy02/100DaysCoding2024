b7777ac5d0ad0c8e7bedb3b74d8e3b1d923512ba
Murpy02 <murpy02022005@gmail.com>
2024-11-17 20:57:05 +0800

    Add files via upload

---
+++ Days77.java
@@ -0,0 +1,13 @@
+package DaysOfCoding;
+
+public class Days77 {
+
+    public static void main(String[] args) {
+        String a = "Majene";
+        String b = "Polewali";
+        String c = "Majene";
+
+        System.out.println("apakah a sama dengan b : " + a.equals(b));
+        System.out.println("apakah a sama dengan c : " + a.equals(c));
+    }
+}
