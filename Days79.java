da065d4ab288678af333170f5b6c1f9705fc476a
Murpy02 <murpy02022005@gmail.com>
2024-11-19 21:18:06 +0800

    Add files via upload

---
+++ Days79.java
@@ -0,0 +1,12 @@
+package DaysOfCoding;
+
+public class Days79 {
+
+    public static void main(String[] args) {
+        String a = " Maju ";
+
+        String hasil = a.toString();
+
+        System.out.println("hasil : " + hasil);
+    }
+}
