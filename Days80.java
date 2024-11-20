3cd83e7e19922457e985fed7c129b3694a6abac1
Murpy02 <murpy02022005@gmail.com>
2024-11-20 20:58:30 +0800

    Add files via upload

---
+++ Days80.java
@@ -0,0 +1,12 @@
+
+package DaysOfCoding;
+
+
+public class Days80 {
+    public static void main(String[] args) {
+        String nama = "  DIMAS  ";
+        System.out.println("Sebelum trim : " + nama);
+        String hasil = nama.trim();
+        System.out.println("Setelah trim : " + hasil);
+    }
+}
