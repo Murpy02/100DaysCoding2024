584f9f5f2fd12480770c8801c4c0a39920d3c6ed
Murpy02 <murpy02022005@gmail.com>
2024-11-21 19:49:38 +0800

    Add files via upload

---
+++ Days81.java
@@ -0,0 +1,18 @@
+
+package DaysOfCoding;
+
+
+public class Days81 {
+    public static void main(String[] args) {
+        int rangka = 123;
+        double pecahan = 10.2;
+       
+        
+        String Angka=String.valueOf(rangka);
+        String Pecahan = String.valueOf(pecahan);
+        
+        System.out.println("String dari int ="+ Angka);
+        System.out.println("String dari int ="+ Pecahan);
+      
+    }
+}
