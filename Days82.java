b054cb8a2d1fa08dc2b3d7d849d8d673d044e2cb
Murpy02 <murpy02022005@gmail.com>
2024-11-22 19:11:37 +0800

    Add files via upload

---
+++ Days82.java
@@ -0,0 +1,33 @@
+
+package DaysOfCoding;
+
+
+public class Days82 {
+    public static void main(String[] args) {
+        //Method ceil()
+        double angka = 8.1;
+        double hasil = Math.ceil(angka);// Membulatkan ke atas
+        System.out.println("Math.ceil("+ angka + ")=" + hasil);
+        
+        System.out.println(" ");
+   // Method floor()
+    double a = 2.2;
+    double h = Math.floor(a);// Membulatkan ke bawah
+    System.out.println("Math.floor("+ a + ")= " + h);
+    
+        System.out.println(" ");
+    //Method round()
+    double a1 = 2.4;
+    double a2 = 4.7;
+    long h1 = Math.round(a1);
+    long h2 = Math.round(a2);
+    
+      
+    
+        System.out.println("Math.round("+ a1 +")=" + h1);
+        System.out.println("Math.round("+ a2 +")=" + h2);
+    
+    
+    }
+    
+}
