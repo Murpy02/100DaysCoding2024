c34504c17594e15d6bc8b33c6e941d2478bd36ce
Murpy02 <murpy02022005@gmail.com>
2024-11-23 23:17:31 +0800

    Add files via upload

---
+++ Days83.java
@@ -0,0 +1,20 @@
+
+package DaysOfCoding;
+
+
+public class Days83 {
+    public static void main(String[] args) {
+        int[] angka = new int[5];
+        
+        angka[0]=10;
+        angka[1]=20;
+        angka[2]=30;
+        angka[3]=40;
+        angka[4]=50;
+        
+        for(int i = 0 ; i < angka.length;i++){
+            System.out.println("Elemen ke " + (i+1) + " : " + angka[i]);
+        }
+    }
+ 
+}
