c99b462679fcc851426e6d7b1997bc69d9f5e5ef
Murpy02 <murpy02022005@gmail.com>
2024-11-25 22:09:52 +0800

    Add files via upload

---
+++ Days85.java
@@ -0,0 +1,14 @@
+
+package DaysOfCoding;
+
+
+public class Days85 {
+    public static void main(String[] args) {
+        int[] array = { 10,20,30,40,50};
+        
+        System.out.println("Isi Array : ");
+        for(int i =0; i<array.length;i++){
+            System.out.println("Elemen ke- "+i+ " : "+ array[i]);
+        }
+    }
+}
