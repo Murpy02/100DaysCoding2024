9dba1388b0d141d3ced24f03d4c16914567f35ff
Murpy02 <murpy02022005@gmail.com>
2024-11-27 18:45:57 +0800

    Add files via upload

---
+++ Days86.java
@@ -0,0 +1,17 @@
+
+package DaysOfCoding;
+
+
+public class Days86 {
+    public static void main(String[] args) {
+        int[] angka = {1,2,3,4,5};
+        
+          System.out.println("Elemen-Elemen dari angka adalah : ");
+        int i =0;
+      while(i<angka.length){
+          System.out.println(angka[i]);
+          i++;
+      }
+        
+    }
+}
