d9667d147211e32a5075aec7738e1e34e2b9ff2c
Murpy02 <murpy02022005@gmail.com>
2024-11-29 19:59:25 +0800

    Add files via upload

--- Days89.java
+++ Days89.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DaysOfCoding;
 
-/**
- *
- * @author Murpy
- */
+import java.util.Scanner;
 public class Days89 {
-    
+    public static void main(String[] args) {
+          Scanner a = new Scanner(System.in);
+        while (true) {
+            System.out.println("masukkan angka : ");
+            String n = a.nextLine();
+            if (n.equalsIgnoreCase("stop")) {
+                System.out.println("program selesai.");
+                break;
+
+            }
+            try {
+                int angka = Integer.parseInt(n);
+                if (angka >= 1 && angka <= 100) {
+                    System.out.println("nilai tidak ditemukan");
+                } else {
+                    System.out.println("tidak ditemukan");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("harap masukkan angka yang valid");
+            }
+
+        }
+    }
 }
+    
+
